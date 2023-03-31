23f36c4fbc4c574037dd33f4f01a6e1232a75ba2
Amit Pawar <amitpawar7475@gmail.com>
2023-04-01 00:16:48 +0530

    add navbar, section

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,15 @@ import React from "react";
 function Card(props) {
     return (
         <div className="card">
-            <img className="card-image" src={`../images/${props.img}`} />
-            <div className="card-stats">
-                <img className="card-star" src="../images/star.png" />
+            <img className="card--image" src={`../images/${props.img}`} />
+            <div className="card--stats">
+                <img className="card--star" src="../images/star.png" />
                 <span>{props.rating}</span>
                 <span className="grey">({props.reviewCount}) • </span>
                 <span className="grey">{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p  className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,9 @@ import React from "react";
 function Hero() {
     return (
         <section className="hero">
-            <img className="hero-image" src="../images/hero-img.png" />
-            <h1 className="hero-header">Online Experiences</h1>
-            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            <img className="hero--photo" src="../images/hero-img.png" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
         </section>
     )
